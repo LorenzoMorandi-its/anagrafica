40dcef77afac9ebf14fd28dbbc72bcff524d0867
LorenzoMorandi-its <lorenzo.morandi@itsrizzoli.it>
2024-10-14 10:42:27 +0200

    Aggiungere i file di progetto.

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AnagraficaApp.Model
+{
+    public class Persona
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public DateTime DataNascita { get; set; }
+        public string Indirizzo { get; set; }
+        public string Citta { get; set; }
+        public string CAP { get; set; }
+        public string Telefono { get; set; }
+
+        public Persona(int id, string nome, string cognome)
+        {
+            ID = id;
+            Nome = nome;
+            Cognome = cognome;
+        }
+    }
+}
---
+++ anagrafica/My Project/AssemblyInfo.vb
@@ -0,0 +1,28 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Le informazioni generali relative a un assembly sono controllate dal seguente 
+' set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+' associate a un assembly.
+
+' Controllare i valori degli attributi degli assembly
+
+<Assembly: AssemblyTitle("anagrafica")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("anagrafica")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+' Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+'
+'      Versione principale
+'      Versione secondaria 
+'      Numero di build
+'      Revisione
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: ComVisible(False)>
